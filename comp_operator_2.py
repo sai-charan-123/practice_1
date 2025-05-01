254a6bdd7f147dfd73198ac7a3530fab3f883034
sai-charan-123 <saicharan271305@gmail.com>
2025-05-01 18:53:53 +0530

    comparision_operator

---
+++ comp_operator_2.py
@@ -0,0 +1,17 @@
+#comparision operator
+a=[1,2,3,4,5,6]
+b=[6,5,4,3,2,1]
+c=[1,3,5]
+d=[2,4,6]
+e=a<b
+print(e)
+e=b<c
+print(e)
+e=c<d
+print(e)
+e=a<c
+print(e)
+e=a<d
+print(e)
+e=b<d
+print(e)
---
+++ comp_operator_3.py
@@ -0,0 +1,17 @@
+#comparision operator
+a=[1,2,3,4,5,6]
+b=[6,5,4,3,2,1]
+c=[1,3,5]
+d=[2,4,6]
+r=a==b
+print(r)
+r=b==c
+print(r)
+r=c==d
+print(r)
+r=a==c
+print(r)
+r=a==d
+print(r)
+r=b==d
+print(r)
